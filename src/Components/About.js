5cbf1f4d820b606a25467f35d88943e093084ded
sefinansa <sefinansa@gmail.com>
2022-02-09 14:06:47 -0300

    land

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,45 +2,33 @@ import React, { Component } from "react";
 
 class About extends Component {
   render() {
-    if (this.props.data) {
-      var name = this.props.data.firstname + " " + this.props.data.lastname;
-      var profilepic = "images/" + this.props.data.image;
-      var bio = this.props.data.bio;
-      var email = this.props.data.email;
-      var linkedInUrl = this.props.data.linkedinurl;
-    }
-
     return (
       <section id="about">
         <div className="row">
           <div className="three columns">
             <img
               className="profile-pic"
-              src={profilepic}
+              src="https://cdn-icons-png.flaticon.com/512/173/173222.png"
               alt="Ahmed Hamodi Profile Pic"
             />
           </div>
           <div className="nine columns main-col">
-            <h2>About Me</h2>
-            <p>{bio}</p>
+            <h2>Esperança</h2>
+            <p>
+              Ter esperança para objetivos na vida é primordial. Não desistir de
+              tentar e sempre buscar o melhor de si mesmo, é primordial para
+              alcançar o sucesso.
+            </p>
             <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{name}</span>
-                  <br />
-                  <span>{email}</span>
-                </p>
-              </div>
               <div className="columns download">
                 <p>
                   <a
-                    href={linkedInUrl}
+                    href="https://sefinan.com.br/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="button"
                   >
-                    <i className="fa fa-download"></i>Find Me on LinkedIn
+                    SIMULE AQUI
                   </a>
                 </p>
               </div>
